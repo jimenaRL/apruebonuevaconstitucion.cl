f34871645019b0ef0c7ca28b0d63d307a07e1cfd
Sergio Sepúlveda Montealegre <sergio.sepulveda@continuumhq.com>
2022-06-15 20:19:05 -0400

    feat: new component carousel-controls-inner

---
+++ src/app/carousel-controls-inner/carousel-controls-inner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'carousel-controls-inner',
+  templateUrl: './carousel-controls-inner.component.html',
+  styleUrls: ['./carousel-controls-inner.component.css']
+})
+export class CarouselControlsInnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
