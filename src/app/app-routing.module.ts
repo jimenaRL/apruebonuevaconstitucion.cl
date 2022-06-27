db182144fb7e289888e8b371f17628dd36a04e88
Sergio Sepúlveda Montealegre <sergio.sepulveda@continuumhq.com>
2022-06-27 12:28:34 -0400

    feat: route kit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { HomeComponent } from './home/home.component';
 import { enableProdMode } from '@angular/core';
+import { KitComponent } from './kit/kit.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
+  { path: 'kit', component: KitComponent},
   { path: 'articles', component: ArticleListComponent, pathMatch: 'full'},
   { path: '**', redirectTo: '/home' },
 ];
